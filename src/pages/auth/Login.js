a0d3069aa68759e12e534332f6dfbf100b93af74
Harry MacCode <emarh.harry.code@gmail.com>
2024-02-11 21:42:50 +0100

    Login Component Part 2

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './auth.module.scss';
 import Card from '../../components/card/Card';
+import { RiLoginCircleLine } from "react-icons/ri";
+import { Link } from 'react-router-dom';
+
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleInputChange = () => {};
+
+  const loginUser = () => {};
   return (
     <div className={`container ${styles.auth}`}>
-      <Card></Card>
+      <Card>
+        <div className={styles.form}>
+            <div className="--flex-center">
+                <RiLoginCircleLine size={35} color='#999' />
+            </div>
+            <h2>Login</h2>
+            <div className="--flex-center">
+                <button className="--btn --btn-google">Login with Google</button>
+            </div>
+            <br />
+            <p className="--text-center --fw-bold">or</p>
+            <form action="">
+                <input type="email" placeholder='Enter your email' required name='email' value={email} onChange={handleInputChange} />
+                <input type="password" placeholder='Enter your password' required name='password' value={password} onChange={handleInputChange} />
+
+                <button type="submit" className='--btn --btn-primary --btn-block'>Login</button>
+            </form>
+            <Link to='/forgot'>Forgot password</Link>
+            <span className={styles.register}>
+                <Link to='/'>Home</Link>
+                <p>&nbsp; Don't have an account ? &nbsp;</p>
+                <Link to='/register'>Register</Link>
+            </span>
+        </div>
+      </Card>
     </div>
   )
 }
