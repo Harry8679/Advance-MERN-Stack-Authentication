38007c697c9d7906ecf2c182da6879f91fa7f1c5
Harry MacCode <emarh.harry.code@gmail.com>
2024-02-11 20:54:01 +0100

    Login Component Part 1 (Card component created)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home/Home';
 import Layout from './components/layout/Layout';
+import Login from './pages/auth/Login';
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <Router>
         <Routes>
           <Route path='/' element={<Layout><Home /></Layout>} />
+          <Route path='/login' element={<Login />} />
         </Routes>
       </Router>
     </>
