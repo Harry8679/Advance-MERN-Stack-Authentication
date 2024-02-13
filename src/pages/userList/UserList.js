d02928526f3c7b227948a06c62976a034eec9f79
Harry MacCode <emarh.harry.code@gmail.com>
2024-02-13 19:53:40 +0100

    User Table

--- src/pages/userList/UserList.js
+++ src/pages/userList/UserList.js
@@ -1,7 +1,10 @@
-import React from 'react'
+import React from 'react';
+import './UserList.scss';
 import PageMenu from '../../components/pageMenu/PageMenu';
 import UserStats from '../../components/userStats/UserStats';
 import Search from '../../components/search/Search';
+import { FaRegTrashCan } from "react-icons/fa6";
+
 
 const UserList = () => {
   return (
@@ -11,13 +14,41 @@ const UserList = () => {
         <UserStats />
 
         <div className="user-list">
-            <div className="--flex-between">
-                <span>
-                    <h3>All Users</h3>
-                </span>
-                <span>
-                    <Search />
-                </span>
+            <div className="table">
+                <div className="--flex-between">
+                    <span>
+                        <h3>All Users</h3>
+                    </span>
+                    <span>
+                        <Search />
+                    </span>
+                </div>
+                <table>
+                <thead>
+                    <tr>
+                        <th>s/n</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Role</th>
+                        <th>Change Role</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Rudolph</td>
+                        <td>rudolph.ebang@gmail.com</td>
+                        <td>Admin</td>
+                        <td>ChangeRole</td>
+                        <td>
+                            <span>
+                                <FaRegTrashCan size={20} color='red' />
+                            </span>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
             </div>
         </div>
       </div>
