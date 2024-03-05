3acd94f377fd7233b6311e8c4c5e3a4fb59defc4
Harry MacCode <emarh.harry.code@gmail.com>
2024-03-05 04:12:01 +0100

    Setup Redux Toolkit - Part 1

--- src/redux/features/auth/authSlice.js
+++ src/redux/features/auth/authSlice.js
@@ -2,14 +2,26 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
     isLoggedIn: false,
+    user: null,
+    users: [],
 }
 
 const authSlice = createSlice({
     name: 'auth',
     initialState,
-    reducers: {}
+    reducers: {
+        RESET(state) {
+            state.twoFactors = false;
+            state.isError = false;
+            state.isSuccess = false;
+            state.isLoading = false;
+            state.message = ""
+        }
+    }
 });
 
-export const {} = authSlice.actions;
+export const { RESET } = authSlice.actions;
+
+export const selectIsLoggedIn = (state) => state.auth.isLoggedIn;
 
 export default authSlice.reducer;
